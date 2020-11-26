9aba572d8bdc107705d26c91b412d34508742538
Ryuichi Watanabe <ryucrosskey@gmail.com>
2020-11-26 18:33:19 +0900

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 
+# curl -Ls https://raw.githubusercontent.com/ryuichi1208/install/main/info.sh | sh
 
 echo -e "\n################## uname -a #############################"
 uname -a
@@ -11,8 +11,8 @@ cat /proc/version
 echo -e "\n################## cat /etc/os-release ##################"
 cat /etc/os-release
 
-echo -e "\n################## ls -l /dev ###########################"
-ls -l /dev
+echo -e "\n################## ls -l / ##############################"
+ls -l /
 
 echo -e "\n################## cat /proc/cpuinfo ####################"
 cat /proc/cpuinfo
@@ -35,8 +35,23 @@ ip a sh
 echo -e "\n################## ip r sh ##############################"
 ip r sh
 
+echo -e "\n################## netstat -antupo ######################"
+netstat -antupo
+
 echo -e "\n################## id ###################################"
 id
 
 echo -e "\n################## curl httpbin.org/ip ##################"
 curl httpbin.org/ip
+
+echo -e "\n################## cat /etc/sysconfig/network ###########"
+cat /etc/sysconfig/network
+
+echo -e "\n################## cat /etc/hosts #######################"
+cat /etc/hosts
+
+echo -e "\n################## cat /etc/resolv.conf #################"
+cat /etc/resolv.conf
+
+echo -e "\n################## cat /etc/nsswitch.conf ###############"
+cat /etc/nsswitch.conf
